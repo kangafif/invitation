d16415864ab92ba0c774370d9537defe516cdef7
Kang Afif <kangafif999@gmail.com>
2025-02-13 17:46:05 +0700

    update_invitation

--- storage/framework/views/15f299f30b7d042a14726fc4eba96b2b.php
+++ storage/framework/views/15f299f30b7d042a14726fc4eba96b2b.php
@@ -216,8 +216,8 @@ Users List
                                                     <div class="p-4 rounded-3 mb-3" style="background: url('<?php echo e(asset('build/images/bg-ungu-emas.jpg')); ?>') no-repeat center center; background-size: cover;">
                                                         <div class="d-flex">
                                                             <div class="flex-grow-1 text-light text-center">
-                                                                <h4 class="text-warning mb-0">Launcing ATM Emas (VM)</h4>
-                                                                    <h5 class="text-light mb-2">Keemasan45</h5>
+                                                                <h4 class="text-warning mb-0">Kemilau KeEmasan45</h4>
+                                                                <h5 class="text-light mb-2">Menuju Indonesia Emas</h5>
                                                                 <img src="<?php echo e(asset('build/images/logo-keemasan.png')); ?>" width="100" class="mb-2">
                                                             </div>
                                                             <div class="flex-shrink-0 text-light">
@@ -238,9 +238,9 @@ Users List
                                                             <div class="col-8">
                                                                 <div>
                                                                     <div id="cvc-elem" class="fw-medium fs-14 text-light">
-                                                                        Gedung Graha Selaras<br>
-                                                                        Jl. Asia Afrika no.133<br>
-                                                                        Bandung, Jawa Barat
+                                                                        Gedung Sate Bandung<br>
+                                                                        15 Februari 2025<br>
+                                                                        Jam 18.30 s/d selesai
                                                                     </div>
                                                                 </div>
                                                             </div>
